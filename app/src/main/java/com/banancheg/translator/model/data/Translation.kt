161c47660f50cf063d52c3a0fb4c882bc71095a3
banancheg <artemmakuwev@gmail.com>
2022-03-29 20:40:46 +0500

    refactor: move hardcoded strings to variables

--- app/src/main/java/com/banancheg/translator/model/data/Translation.kt
+++ app/src/main/java/com/banancheg/translator/model/data/Translation.kt
@@ -2,4 +2,8 @@ package com.banancheg.translator.model.data
 
 import com.google.gson.annotations.SerializedName
 
-class Translation(@field:SerializedName("text") val translation: String?)
+class Translation(@SerializedName(FIELD_TRANSLATION) val translation: String?) {
+    companion object {
+        private const val FIELD_TRANSLATION = "text"
+    }
+}
