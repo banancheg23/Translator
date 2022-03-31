737d2cc291bd281a6d7cc355316e5a8eff0e4b19
banancheg <artemmakuwev@gmail.com>
2022-03-31 16:10:52 +0500

    add ActivityModule

---
+++ app/src/main/java/com/banancheg/translator/di/ActivityModule.kt
@@ -0,0 +1,12 @@
+package com.banancheg.translator.di
+
+import com.banancheg.translator.view.main.MainActivity
+import dagger.Module
+import dagger.android.ContributesAndroidInjector
+
+@Module
+abstract class ActivityModule {
+
+    @ContributesAndroidInjector
+    abstract fun contributeMainActivity(): MainActivity
+}
