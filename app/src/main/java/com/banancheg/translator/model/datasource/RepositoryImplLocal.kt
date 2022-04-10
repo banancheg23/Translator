2be1e95c84ae5881b8112c2b7f3e48cef90e8127
banancheg <artemmakuwev@gmail.com>
2022-04-10 22:36:35 +0500

    implement local repository

---
+++ app/src/main/java/com/banancheg/translator/model/datasource/RepositoryImplLocal.kt
@@ -0,0 +1,14 @@
+package com.banancheg.translator.model.datasource
+
+import com.banancheg.translator.model.data.AppState
+import com.banancheg.translator.model.data.DataModel
+
+class RepositoryImplLocal(private val dataSource: DataSourceLocal<List<DataModel>>) : RepositoryLocal<List<DataModel>> {
+    override suspend fun getData(word: String): List<DataModel> {
+        return dataSource.getData(word)
+    }
+
+    override suspend fun saveToDb(appState: AppState) {
+        dataSource.saveToDb(appState)
+    }
+}
--- app/src/main/java/com/banancheg/translator/model/datasource/RepositoryLocal.kt
+++ app/src/main/java/com/banancheg/translator/model/datasource/RepositoryLocal.kt
@@ -1,9 +1,7 @@
 package com.banancheg.translator.model.datasource
 
 import com.banancheg.translator.model.data.AppState
-import com.banancheg.translator.model.data.DataModel
-import io.reactivex.Observable
 
-interface DataSourceLocal<T> : DataSource<T> {
+interface RepositoryLocal<T> : Repository<T> {
     suspend fun saveToDb(appState: AppState)
 }
