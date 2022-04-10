b072a89befb0f9016a3e9aaa72858cbb19a4f8a6
banancheg <artemmakuwev@gmail.com>
2022-04-10 22:35:00 +0500

    add TranslatorDataBase, HistoryEntity, HistoryDao

---
+++ app/src/main/java/com/banancheg/translator/db/HistoryEntity.kt
@@ -0,0 +1,14 @@
+package com.banancheg.translator.db
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.Index
+import androidx.room.PrimaryKey
+
+@Entity(indices = [Index(value = arrayOf("word"), unique = true)])
+class HistoryEntity (
+    @PrimaryKey
+    @ColumnInfo(name = "word") var word: String,
+    @ColumnInfo(name = "description") var description: String?
+)
+
